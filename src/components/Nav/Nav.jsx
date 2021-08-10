5b93d2e98906045aa3dc3229d503afbbc080d565
rious275 <rious275@naver.com>
2021-08-10 11:14:27 +0900

    - Add: Nav 레이아웃 완료

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,79 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <NavSection>
+      <ContentContainer>
+        <Logo href="/">LETTER LOG</Logo>
+        <NavRight>
+          <IconSearch>
+            <i class="fas fa-search"></i>
+          </IconSearch>
+          <InputSearch placeholder="우체통을 검색해주세요." />
+          <CreatePostBtn>우체통 만들기</CreatePostBtn>
+        </NavRight>
+      </ContentContainer>
+    </NavSection>
+  );
 };
 
 export default Nav;
+
+const NavSection = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.05);
+  z-index: 999;
+`;
+
+const ContentContainer = styled.div`
+  ${({ theme }) => theme.setFlex('space-between')}
+  height: 111px;
+  padding: 0 8vw;
+`;
+
+const Logo = styled.a`
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.fontColor};
+`;
+
+const NavRight = styled.div`
+  ${({ theme }) => theme.setFlex('stretch')}
+`;
+
+const IconSearch = styled.div`
+  &:hover {
+    cursor: pointer;
+  }
+
+  i {
+    font-size: 20px;
+    color: ${({ theme }) => theme.fontColor};
+  }
+`;
+
+const InputSearch = styled.input`
+  font-size: 16px;
+  margin: 0 10px;
+  border-style: none;
+  border-bottom: 1px solid ${({ theme }) => theme.fontColor};
+  outline: none;
+
+  &::placeholder {
+    font-weight: 300;
+    color: ${({ theme }) => theme.subFontColor};
+  }
+`;
+
+const CreatePostBtn = styled.p`
+  margin: 0 10px;
+  color: ${({ theme }) => theme.fontColor};
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
