b89fdb97088903a33bfb3115fab6a676b06a2834
Junghoon-P <qkr0454@naver.com>
2021-09-29 15:23:54 +0900

    backend sever fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://13.124.4.250:8000';
+const BASE_URL = 'http://15.165.237.210:8000';
 // const BASE_URL = 'http://10.58.0.30:8000';
 
 export const POSTBOXES_API = `${BASE_URL}/postboxes`;
